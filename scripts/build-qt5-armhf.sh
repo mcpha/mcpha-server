eba81df681510b3cd3ba930cb5de988afe04fc6e
Pavel Demin <pavel-demin@outlook.com>
2017-01-15 15:55:20 +0100

    remove deprecated qt modules from build-qt5-armhf.sh

--- scripts/build-qt5-armhf.sh
+++ scripts/build-qt5-armhf.sh
@@ -13,7 +13,7 @@ cp -a patches/linux-arm-gnueabihf-g++ ${qt_dir}/qtbase/mkspecs/
 
 cd ${qt_dir}
 
-./configure -release -static -largefile -opensource -confirm-license -prefix ${qt_pfx} -xplatform linux-arm-gnueabihf-g++ -qt-pcre -qt-zlib -no-audio-backend -no-cups -no-nis -no-dbus -no-xcb -no-linuxfb -no-fontconfig -no-evdev -no-iconv -no-gif -no-libpng -no-libjpeg -no-compile-examples -no-gui -no-widgets -no-opengl -no-icu -no-eglfs -no-sql-sqlite -no-sql-sqlite2 -no-freetype -no-harfbuzz -no-openssl -no-xkbcommon -no-pulseaudio -no-alsa -no-gtkstyle -no-qml-debug -skip qtquick1 -skip qtdeclarative -nomake examples -nomake tests
+./configure -release -static -largefile -opensource -confirm-license -prefix ${qt_pfx} -xplatform linux-arm-gnueabihf-g++ -qt-pcre -qt-zlib -no-audio-backend -no-cups -no-dbus -no-xcb -no-linuxfb -no-fontconfig -no-evdev -no-iconv -no-gif -no-libpng -no-libjpeg -no-compile-examples -no-gui -no-widgets -no-opengl -no-icu -no-eglfs -no-sql-sqlite -no-sql-sqlite2 -no-freetype -no-harfbuzz -no-openssl -no-xkbcommon -no-pulseaudio -no-alsa -no-qml-debug -skip qtdeclarative -nomake examples -nomake tests
 
 make module-qtwebsockets
 make module-qtwebsockets-install_subtargets
