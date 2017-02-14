e7d2698a8275d388f099eb9c1c8031d47610923f
Pavel Demin <pavel-demin@outlook.com>
2017-02-14 17:31:06 +0100

    switch to Qt 5.8.0

--- scripts/build-qt5-armhf.sh
+++ scripts/build-qt5-armhf.sh
@@ -1,7 +1,7 @@
-qt_ver=5.7.1
+qt_ver=5.8.0
 qt_dir=qt-everywhere-opensource-src-${qt_ver}
 qt_tar=${qt_dir}.tar.gz
-qt_url=http://download.qt.io/official_releases/qt/5.7/${qt_ver}/single/${qt_tar}
+qt_url=http://download.qt.io/official_releases/qt/5.8/${qt_ver}/single/${qt_tar}
 
 qt_pfx=$PWD/qt-${qt_ver}-armhf
 
@@ -13,7 +13,7 @@ cp -a patches/linux-arm-gnueabihf-g++ ${qt_dir}/qtbase/mkspecs/
 
 cd ${qt_dir}
 
-./configure -release -static -largefile -opensource -confirm-license -prefix ${qt_pfx} -xplatform linux-arm-gnueabihf-g++ -qt-pcre -qt-zlib -no-audio-backend -no-cups -no-dbus -no-xcb -no-linuxfb -no-fontconfig -no-evdev -no-iconv -no-gif -no-libpng -no-libjpeg -no-compile-examples -no-gui -no-widgets -no-opengl -no-icu -no-eglfs -no-sql-sqlite -no-sql-sqlite2 -no-freetype -no-harfbuzz -no-openssl -no-xkbcommon -no-pulseaudio -no-alsa -no-qml-debug -skip qtdeclarative -nomake examples -nomake tests
+./configure -release -static -opensource -confirm-license -prefix ${qt_pfx} -xplatform linux-arm-gnueabihf-g++ -qt-pcre -qt-zlib -no-accessibility -no-cups -no-dbus -no-xcb -no-linuxfb -no-fontconfig -no-evdev -no-iconv -no-gif -no-libpng -no-libjpeg -no-compile-examples -no-gui -no-widgets -no-opengl -no-icu -no-eglfs -no-sql-sqlite -no-sql-sqlite2 -no-freetype -no-harfbuzz -no-openssl -no-xkbcommon -no-pulseaudio -no-alsa -no-qml-debug -skip qtdeclarative -nomake examples -nomake tests
 
 make module-qtwebsockets
 make module-qtwebsockets-install_subtargets
